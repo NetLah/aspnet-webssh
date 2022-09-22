9f671bfe3bbff98bedb364624e988007ac061995
Tho Ho <code@tho.email>
2022-09-22 16:00:37 +0800

    Use PRETTY_NAME on /etc/os-release

--- eng/debian/src/opt/startup/init_container.sh
+++ eng/debian/src/opt/startup/init_container.sh
@@ -5,7 +5,7 @@ cat >/etc/motd <<EOL
   / _ \\\__ \  _/| .\` | _|  | |   | (__/ _ \ '_/ -_)
  /_/ \_\___/_|(_)_|\_|___| |_|    \___\___/_| \___|
 
-$(source /usr/lib/os-release;echo $PRETTY_NAME)
+$(source /etc/os-release;echo $PRETTY_NAME)
 ASP.NET Core v${ASPNET_VERSION}
 EOL
 cat /etc/motd
